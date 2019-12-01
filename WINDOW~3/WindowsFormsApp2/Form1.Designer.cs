3c0682eb6d92f3330effc2c27a8760ef7c3ede90
lsj <sneak7581@wku.ac.kr>
2019-12-01 21:06:38 +0900

    주석처리, 자동저장

---
+++ WINDOW~3/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Header_name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Header_number = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button_remove = new System.Windows.Forms.Button();
+            this.button_add = new System.Windows.Forms.Button();
+            this.button_modi = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.파일FToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.열ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.저장SToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.종료XToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Header_name,
+            this.Header_number});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(664, 18);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(290, 310);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.ListView1_SelectedIndexChanged);
+            // 
+            // Header_name
+            // 
+            this.Header_name.Text = "이름";
+            this.Header_name.Width = 100;
+            // 
+            // Header_number
+            // 
+            this.Header_number.Text = "전화번호";
+            this.Header_number.Width = 100;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(114, 144);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 28);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(437, 144);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(168, 28);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(50, 147);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 28);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "이름";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(330, 147);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 28);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "전화번호";
+            // 
+            // button_remove
+            // 
+            this.button_remove.Location = new System.Drawing.Point(437, 231);
+            this.button_remove.Name = "button_remove";
+            this.button_remove.Size = new System.Drawing.Size(127, 36);
+            this.button_remove.TabIndex = 13;
+            this.button_remove.Text = "삭제하기";
+            this.button_remove.UseVisualStyleBackColor = true;
+            this.button_remove.Click += new System.EventHandler(this.Button_remove_Click);
+            // 
+            // button_add
+            // 
+            this.button_add.Location = new System.Drawing.Point(117, 231);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(127, 36);
+            this.button_add.TabIndex = 11;
+            this.button_add.Text = "추가하기";
+            this.button_add.UseVisualStyleBackColor = true;
+            this.button_add.Click += new System.EventHandler(this.Button_add_Click);
+            // 
+            // button_modi
+            // 
+            this.button_modi.Location = new System.Drawing.Point(277, 231);
+            this.button_modi.Name = "button_modi";
+            this.button_modi.Size = new System.Drawing.Size(127, 36);
+            this.button_modi.TabIndex = 12;
+            this.button_modi.Text = "수정하기";
+            this.button_modi.UseVisualStyleBackColor = true;
+            this.button_modi.Click += new System.EventHandler(this.Button_modi_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일FToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(970, 36);
+            this.menuStrip1.TabIndex = 16;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 파일FToolStripMenuItem
+            // 
+            this.파일FToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.열ToolStripMenuItem,
+            this.저장SToolStripMenuItem,
+            this.종료XToolStripMenuItem});
+            this.파일FToolStripMenuItem.Name = "파일FToolStripMenuItem";
+            this.파일FToolStripMenuItem.Size = new System.Drawing.Size(83, 32);
+            this.파일FToolStripMenuItem.Text = "파일(&F)";
+            // 
+            // 열ToolStripMenuItem
+            // 
+            this.열ToolStripMenuItem.Name = "열ToolStripMenuItem";
+            this.열ToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.열ToolStripMenuItem.Text = "열기(&O)...";
+            this.열ToolStripMenuItem.Click += new System.EventHandler(this.열ToolStripMenuItem_Click);
+            // 
+            // 저장SToolStripMenuItem
+            // 
+            this.저장SToolStripMenuItem.Name = "저장SToolStripMenuItem";
+            this.저장SToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.저장SToolStripMenuItem.Text = "저장(&S)";
+            this.저장SToolStripMenuItem.Click += new System.EventHandler(this.저장SToolStripMenuItem_Click);
+            // 
+            // 종료XToolStripMenuItem
+            // 
+            this.종료XToolStripMenuItem.Name = "종료XToolStripMenuItem";
+            this.종료XToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.종료XToolStripMenuItem.Text = "종료(&X)";
+            this.종료XToolStripMenuItem.Click += new System.EventHandler(this.종료XToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 350);
+            this.Controls.Add(this.button_remove);
+            this.Controls.Add(this.button_add);
+            this.Controls.Add(this.button_modi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ColumnHeader Header_name;
+        private System.Windows.Forms.ColumnHeader Header_number;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button_remove;
+        private System.Windows.Forms.Button button_add;
+        private System.Windows.Forms.Button button_modi;
+        public System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 파일FToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 열ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 저장SToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 종료XToolStripMenuItem;
+    }
+}
+
---
+++ WINDOW~3/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_add_Click(object sender, EventArgs e) //추가하기 버튼ㅎ
+        {
+            if (textBox1.Text == "" || textBox2.Text == "") //이름 or 번호 입력 안됐을 때 출력 메세지
+            {
+                MessageBox.Show("이름또는 전화번호가 입력되지 않았습니다.");
+            }
+
+            else //이름, 번호 입력 됐을 때 리스트 뷰에 추가
+            {
+
+                string name = textBox1.Text;
+                string number = textBox2.Text;
+
+                string[] strs = new string[] { name, number };
+                ListViewItem item = new ListViewItem(strs);
+
+                item.Text = name;
+                listView1.Items.Add(item);
+                ClearInputControl();
+                SaveContact();
+
+                MessageBox.Show("전화번호부를 저장 하였습니다.");
+            }
+        }
+
+        private void ClearInputControl() //저장하거나 수정 ,삭제한 뒤 텍스트 박스 비우기
+        {
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+        }
+
+        private void SaveContact() //추가, 수정, 삭제 뒤 txt파일에 즉각적으로 저장
+        {
+            StreamWriter sw = new StreamWriter("test.txt");
+
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                sw.WriteLine(listView1.Items[i].Text + "|" + listView1.Items[i].SubItems[1].Text + "|");
+            }
+            sw.Close();
+        }
+
+        private void ListView1_SelectedIndexChanged(object sender, EventArgs e) // ??
+        {
+            bool selected = listView1.SelectedItems.Count > 0;
+            button_remove.Enabled = button_modi.Enabled = selected;
+            if (selected == false)
+            {
+                ClearInputControl();
+                return;
+            }
+            ListViewItem lvi = listView1.SelectedItems[0];
+            textBox1.Text = lvi.SubItems[0].Text;
+            textBox2.Text = lvi.SubItems[1].Text;
+        }
+
+        private void Button_modi_Click(object sender, EventArgs e) //수정버튼 
+        {
+            bool selected = listView1.SelectedItems.Count > 0;
+            if (selected == false)
+            {
+                MessageBox.Show("선택되지 않았습니다.");
+            }
+            else
+            {
+                ListViewItem lvi = listView1.SelectedItems[0];
+                string name = textBox1.Text;
+                string number = textBox2.Text;
+                lvi.SubItems[0].Text = name;
+                lvi.SubItems[1].Text = number;
+                ClearInputControl();
+                SaveContact();
+                MessageBox.Show("수정 되었습니다");
+            }
+        }
+
+        private void Button_remove_Click(object sender, EventArgs e) //삭제하기 버튼
+        {
+            bool selected = listView1.SelectedItems.Count > 0;
+            if (selected == false) //선택된거 없을 때
+            {
+                MessageBox.Show("선택되지 않았습니다.");
+            }
+            else //삭제확인
+            {
+                if(MessageBox.Show("정말 삭제 하시겠습니까?", "삭제 확인", MessageBoxButtons.YesNo,MessageBoxIcon.Question)== DialogResult.Yes)
+                { 
+                     ListViewItem lvi = listView1.SelectedItems[0];
+                     listView1.Items.Remove(lvi);
+                     ClearInputControl();
+                     SaveContact();
+                }
+
+            }
+        }
+
+        private void Button_txt_Click(object sender, EventArgs e) //메뉴아이템의 저장버튼 클릭시 이벤트 
+        {
+            StreamWriter sw = new StreamWriter("test.txt");
+
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                sw.WriteLine(listView1.Items[i].Text + "|" + listView1.Items[i].SubItems[1].Text + "|");
+            }
+            sw.Close();
+
+            MessageBox.Show("전화번호부를 저장 하였습니다.");
+
+        }
+
+        private void Button_open_Click(object sender, EventArgs e) //메뉴 아이템의 오픈버튼 클릭시 이벤트
+        {
+            using (TextReader tReader = new StreamReader("test.txt"))
+            {
+                string[] stringLines = tReader.ReadToEnd().Replace("\n", "").Split((char)Keys.Enter);
+
+                foreach (string stringLine in stringLines)
+                {
+                    if (stringLine != string.Empty)
+                    {
+                        string[] lineItems = stringLine.Split('|');
+
+                        ListViewItem lv = new ListViewItem();
+                        lv.Text = lineItems[0];
+                        lv.SubItems.Add(lineItems[1]);
+                        listView1.Items.Add(lv);
+                    }
+                }
+            }
+        }
+
+        private void 종료XToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void 저장SToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter("test.txt");
+
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                sw.WriteLine(listView1.Items[i].Text + "|" + listView1.Items[i].SubItems[1].Text + "|");
+            }
+            sw.Close();
+
+            MessageBox.Show("전화번호부를 저장 하였습니다.");
+        }
+
+        private void 열ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (TextReader tReader = new StreamReader("test.txt"))
+            {
+                string[] stringLines = tReader.ReadToEnd().Replace("\n", "").Split((char)Keys.Enter);
+
+                foreach (string stringLine in stringLines)
+                {
+                    if (stringLine != string.Empty)
+                    {
+                        string[] lineItems = stringLine.Split('|');
+
+                        ListViewItem lv = new ListViewItem();
+                        lv.Text = lineItems[0];
+                        lv.SubItems.Add(lineItems[1]);
+                        listView1.Items.Add(lv);
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) //폼 실행 될 때 txt에 저장돼있는 글들 리스트뷰로 띄우기
+        {
+            using (TextReader tReader = new StreamReader("test.txt"))
+            {
+                string[] stringLines = tReader.ReadToEnd().Replace("\n", "").Split((char)Keys.Enter);
+
+                foreach (string stringLine in stringLines)
+                {
+                    if (stringLine != string.Empty)
+                    {
+                        string[] lineItems = stringLine.Split('|');
+
+                        ListViewItem lv = new ListViewItem();
+                        lv.Text = lineItems[0];
+                        lv.SubItems.Add(lineItems[1]);
+                        listView1.Items.Add(lv);
+                    }
+                }
+            }
+        }
+    }
+}
